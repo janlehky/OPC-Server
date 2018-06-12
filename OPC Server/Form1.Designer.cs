d65a43022ea87da74a5c0e90f6c86aceb76b31d1
Jan Lehky <sksejleh@eu.iko>
2018-06-12 20:32:26 +0200

    Add project files.

---
+++ OPC Server/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace OPC_Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnServerStart = new System.Windows.Forms.Button();
+            this.rtTransactions = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.stsLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.ipAddress_form = new System.Windows.Forms.TextBox();
+            this.port_form = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.opcServerAddress = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(12, 84);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(168, 46);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect to RSLinx";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnServerStart
+            // 
+            this.btnServerStart.Location = new System.Drawing.Point(12, 32);
+            this.btnServerStart.Name = "btnServerStart";
+            this.btnServerStart.Size = new System.Drawing.Size(168, 46);
+            this.btnServerStart.TabIndex = 3;
+            this.btnServerStart.Text = "Start Server";
+            this.btnServerStart.UseVisualStyleBackColor = true;
+            this.btnServerStart.Click += new System.EventHandler(this.btnServerStart_Click);
+            // 
+            // rtTransactions
+            // 
+            this.rtTransactions.Location = new System.Drawing.Point(16, 136);
+            this.rtTransactions.Name = "rtTransactions";
+            this.rtTransactions.Size = new System.Drawing.Size(452, 197);
+            this.rtTransactions.TabIndex = 4;
+            this.rtTransactions.Text = "";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stsLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 336);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(480, 25);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // stsLabel
+            // 
+            this.stsLabel.Name = "stsLabel";
+            this.stsLabel.Size = new System.Drawing.Size(35, 20);
+            this.stsLabel.Text = "Info";
+            // 
+            // ipAddress_form
+            // 
+            this.ipAddress_form.Location = new System.Drawing.Point(186, 44);
+            this.ipAddress_form.Name = "ipAddress_form";
+            this.ipAddress_form.Size = new System.Drawing.Size(150, 22);
+            this.ipAddress_form.TabIndex = 8;
+            // 
+            // port_form
+            // 
+            this.port_form.Location = new System.Drawing.Point(342, 44);
+            this.port_form.Name = "port_form";
+            this.port_form.Size = new System.Drawing.Size(52, 22);
+            this.port_form.TabIndex = 9;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // opcServerAddress
+            // 
+            this.opcServerAddress.Location = new System.Drawing.Point(186, 96);
+            this.opcServerAddress.Name = "opcServerAddress";
+            this.opcServerAddress.Size = new System.Drawing.Size(260, 22);
+            this.opcServerAddress.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 361);
+            this.Controls.Add(this.opcServerAddress);
+            this.Controls.Add(this.port_form);
+            this.Controls.Add(this.ipAddress_form);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.rtTransactions);
+            this.Controls.Add(this.btnServerStart);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "OPC Server";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnServerStart;
+        private System.Windows.Forms.RichTextBox rtTransactions;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel stsLabel;
+        private System.Windows.Forms.TextBox ipAddress_form;
+        private System.Windows.Forms.TextBox port_form;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox opcServerAddress;
+    }
+}
+
---
+++ OPC Server/Form1.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Opc.Da;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using System.Net;
+
+namespace OPC_Server
+{
+    public partial class Form1 : Form
+    {
+
+        // variable definition for OPC communication
+        private OpcCom.Factory fact = new OpcCom.Factory();
+        private Server server;
+
+        private SubscriptionState groupState;
+
+        private Subscription group;
+
+        private List<Item> itemList = new List<Item>();
+
+        private TcpClient client;
+        public StreamReader STR;
+        public StreamWriter STW;
+        public string recieve;
+        private Socket listener;
+
+        // Thread signal.  
+        public ManualResetEvent allDone = new ManualResetEvent(false);
+
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            string localIP;
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                socket.Connect("8.8.8.8", 65530);
+                IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                localIP = endPoint.Address.ToString();
+            }
+
+            ipAddress_form.Text = localIP;
+            port_form.Text = "13000";
+            opcServerAddress.Text = "opcda://localhost/RSLinx OPC Server";
+
+            // open socket for communication
+            backgroundWorker1.RunWorkerAsync();
+
+            // connect to OPC server
+            ConnectToOpcServer();
+        }
+
+        private void ConnectToOpcServer()
+        {
+            try
+            {
+                // define communication to server
+                this.server = new Server(fact, null);
+                this.server.Url = new Opc.URL(opcServerAddress.Text);
+
+                // open communication to server
+                this.server.Connect();
+                
+                // create subscription group with set refresh rate
+                groupState = new SubscriptionState();
+                groupState.Name = "Group";
+                groupState.UpdateRate = 1000;
+                groupState.Active = true;
+                this.group = (Opc.Da.Subscription)server.CreateSubscription(groupState);
+                log.Info("Connected to OPC server");
+            }
+            catch (Exception x)
+            {
+                log.Fatal(x.Message);
+                // rtTransactions.Invoke((MethodInvoker)(() => rtTransactions.AppendText(x.Message)));
+            }
+        }
+
+        private void writeValue(Int32 value, string tagName)
+        {
+            try
+            {
+                List<Item> list = this.group.Items.ToList();
+                int index = list.FindIndex(x => x.ItemName == tagName); // x.ItemName.StartsWith(tagName)
+
+                if (index == -1)
+                {
+                    // add PLC item to listv
+                    Item[] items = new Item[1];
+                    items[0] = new Item();
+                    items[0].ItemName = tagName;
+                    items = this.group.AddItems(items);
+                }
+
+                stsLabel.Text = this.group.Items.Length.ToString();
+                // write items
+                ItemValue[] writeValues = new ItemValue[1];
+                writeValues[0] = new ItemValue();
+                if (index == -1)
+                {
+                    writeValues[0].ServerHandle = this.group.Items.Last().ServerHandle;
+                }
+                else
+                {
+                    writeValues[0].ServerHandle = this.group.Items[index].ServerHandle;
+                }
+                writeValues[0].Value = value;
+                Opc.IRequest req;
+                this.group.Write(writeValues, 321, new WriteCompleteEventHandler(WriteCompleteCallback), out req);
+            }
+            catch (Exception x)
+            {
+                log.Fatal(x.Message);
+                // rtTransactions.Invoke((MethodInvoker)(() => rtTransactions.AppendText(x.Message)));
+            }
+        }
+
+        private void writeValue_Float(float value, string tagName)
+        {
+            try
+            {
+                List<Item> list = this.group.Items.ToList();
+                int index = list.FindIndex(x => x.ItemName == tagName); // x.ItemName.StartsWith(tagName)
+
+                if (index == -1)
+                {
+                    // add PLC item to listv
+                    Item[] items = new Item[1];
+                    items[0] = new Item();
+                    items[0].ItemName = tagName;
+                    items = this.group.AddItems(items);
+                }
+
+                stsLabel.Text = this.group.Items.Length.ToString();
+                // write items
+                ItemValue[] writeValues = new ItemValue[1];
+                writeValues[0] = new ItemValue();
+                if (index == -1)
+                {
+                    writeValues[0].ServerHandle = this.group.Items.Last().ServerHandle;
+                }
+                else
+                {
+                    writeValues[0].ServerHandle = this.group.Items[index].ServerHandle;
+                }
+                writeValues[0].Value = value;
+                Opc.IRequest req;
+                this.group.Write(writeValues, 321, new WriteCompleteEventHandler(WriteCompleteCallback), out req);
+            }
+            catch (Exception x)
+            {
+                log.Fatal(x.Message);
+                // rtTransactions.Invoke((MethodInvoker)(() => rtTransactions.AppendText(x.Message)));
+            }
+        }
+
+        private void WriteCompleteCallback(object clientHandle, Opc.IdentifiedResult[] results)
+        {
+            Console.WriteLine("Write completed");
+            foreach (Opc.IdentifiedResult writeResult in results)
+            {
+                // Console.WriteLine("\t{0} write result: {1}", writeResult.ItemName, writeResult.ResultID);
+                log.Info(writeResult.ResultID.ToString());
+                // rtTransactions.Invoke((MethodInvoker)(() => rtTransactions.AppendText(writeResult.ResultID.ToString() + "\n")));
+            }
+            Console.WriteLine();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.server.IsConnected)
+                {
+                    this.server.Disconnect();
+                }
+
+                ConnectToOpcServer();
+            }
+            catch (Exception x)
+            {
+                log.Fatal(x.Message);
+            }
+        }
+
+        private void btnServerStart_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        public void AcceptCallback(IAsyncResult ar)
+        {
+            // Signal the main thread to continue.  
+            allDone.Set();
+
+            // Get the socket that handles the client request.  
+            Socket listener = (Socket)ar.AsyncState;
+            Socket handler = listener.EndAccept(ar);
+
+            // Create the state object.  
+            StateObject state = new StateObject();
+            state.workSocket = handler;
+            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                new AsyncCallback(ReadCallback), state);
+        }
+
+        public void ReadCallback(IAsyncResult ar)
+        {
+            String content = String.Empty;
+
+            // Retrieve the state object and the handler socket  
+            // from the asynchronous state object.  
+            StateObject state = (StateObject)ar.AsyncState;
+            Socket handler = state.workSocket;
+
+            try
+            {
+                // Read data from the client socket.   
+                int bytesRead = handler.EndReceive(ar);
+
+                if (bytesRead > 0)
+                {
+                    // There  might be more data, so store the data received so far.  
+                    state.sb.Append(Encoding.ASCII.GetString(
+                        state.buffer, 0, bytesRead));
+
+                    content = Encoding.ASCII.GetString(state.buffer, 0, bytesRead).ToString();
+                    string[] data = content.Split(new[] { " " }, StringSplitOptions.None);
+
+                    log.Info(content);
+                    // rtTransactions.Invoke((MethodInvoker)(() => rtTransactions.AppendText(content + "\n")));
+
+                    if (data[0] == "w")
+                    {
+                        Int32 int_value;
+                        if (Int32.TryParse(data[2], out int_value)){
+                            writeValue(int_value, data[1]);
+                        }
+                        else
+                        {
+                            writeValue_Float(float.Parse(data[2]), data[1]);
+                        }
+                    }
+
+                    handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                    new AsyncCallback(ReadCallback), state);
+                }
+            }
+            catch(Exception x)
+            {
+                log.Fatal(x.Message);
+                // rtTransactions.Invoke((MethodInvoker)(() => rtTransactions.AppendText(x.Message)));
+            }
+        }
+
+        // State object for reading client data asynchronously  
+        public class StateObject
+        {
+            // Client  socket.  
+            public Socket workSocket = null;
+            // Size of receive buffer.  
+            public const int BufferSize = 1024;
+            // Receive buffer.  
+            public byte[] buffer = new byte[BufferSize];
+            // Received data string.  
+            public StringBuilder sb = new StringBuilder();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Int32 port = Int32.Parse(port_form.Text);
+            
+            // Data buffer for incoming data.  
+            byte[] bytes = new Byte[1024];
+
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = IPAddress.Parse(ipAddress_form.Text);
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
+
+            SocketPermission permission = new SocketPermission(NetworkAccess.Accept,
+                   TransportType.Tcp, "", SocketPermission.AllPorts);
+            listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            log.Info("Socket worker started");
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                while (true)
+                {
+                    // Set the event to nonsignaled state.  
+                    allDone.Reset();
+
+                    // Start an asynchronous socket to listen for connections.  
+                    listener.BeginAccept(
+                        new AsyncCallback(AcceptCallback),
+                        listener);
+
+                    // Wait until a connection is made before continuing.  
+                    allDone.WaitOne();
+                }
+
+            }
+            catch (Exception x)
+            {
+                log.Fatal(x.Message);
+                // rtTransactions.Invoke((MethodInvoker)(() => rtTransactions.AppendText(x.Message)));
+            }
+        }
+    }
+}
